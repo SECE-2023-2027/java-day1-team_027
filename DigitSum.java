4760a5ce6d389ed6059b6702e12653409293018b
vaishnavpv29 <vaishnav.pv2023csbs@sece.ac.in>
2026-06-15 23:15:20 +0530

    9

---
+++ DigitSum.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class DigitSum {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        int n = s.nextInt();
+        int sum = 0;
+
+        while (n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        System.out.println("The sum of the digits is: " + sum);
+    }
+}
