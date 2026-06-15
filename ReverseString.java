d95d61f8ffdecead676e65f5b2ddc0861f39e96e
vaishnavpv29 <vaishnav.pv2023csbs@sece.ac.in>
2026-06-15 23:16:27 +0530

    10

---
+++ ReverseString.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ReverseString {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        String a = s.nextLine();
+        String b = "";
+
+        for (int i = a.length() - 1; i >= 0; i--) {
+            b += a.charAt(i);
+        }
+
+        System.out.println("Reverse string: " + b);
+    }
+}
